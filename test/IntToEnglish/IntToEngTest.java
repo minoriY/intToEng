3291e256c5f31c4fa30983d3c0e334820d70c212
g14958ym <g14958ym+git@gm.tsuda.ac.jp>
2016-05-18 10:29:43 +0900

    ドライバー：g14958、ナビゲーター：minoriY 10000以下の整数を英語表記できるようにした 10000より大きい数には注意を表示

--- test/IntToEnglish/IntToEngTest.java
+++ test/IntToEnglish/IntToEngTest.java
@@ -8,43 +8,38 @@ import org.junit.Test;
 public class IntToEngTest {
 
 	@Test
-	public void ゼロから１０まで() {
+	public void ゼロから100未満() {
 		IntToEng ite = new IntToEng();
-		String expected[] = {"zero","one","two","three","four","five","six"
-		         ,"seven","eight","nine","ten"};
-		String actual[] = new String[11];
-		for(int n = 0; n < 11; n++) {
-		actual[n] = ite.translateEng(n);
-		}
+		String expected = "ninety nine";
+		String actual = ite.underHundred(99);
 		assertThat(actual, is(expected));
 	}
 	
 	@Test
-	public void ゼロから１００まで() {
+	public void ゼロから1000未満() {
 		IntToEng ite = new IntToEng();
-		String num1[] = {"zero","one","two","three","four","five","six","seven","eight","nine","ten",
-			      		 "eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
-		String num2[] = {"twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"};
-		String num3 = "one hundred";
-		String expected[] = new String[101];
-		for(int i = 0; i < 101; i++) {
-			if(i < 20) {
-				expected[i] = num1[i];
-	    	} else if(i < 100) {
-	    		if(i%10 == 0) {
-	    			expected[i] = num2[i/10 - 2];
-	    		} else {
-	    			expected[i] = num2[i/10 - 2] + " " + num1[i%10];
-	    		}
-	    	} else {
-	    		expected[i] = num3; 
-	    	}
-		}
-		String actual[] = new String[101];
-		for(int n = 0; n < 101; n++) {
-			actual[n] = ite.translateEng(n);
-		}
+		String expected = "nine hundred ninety nine";
+		String actual = ite.underThousand(999);
 		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void ゼロから10000未満() {
+		IntToEng ite = new IntToEng();
+		String expected = "nine thousand nine hundred ninety nine";
+		String actual = ite.underTenThousand(9999);
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void ゼロから10000まで() {
+		IntToEng ite = new IntToEng();
+		String expected1 = "ten thousand";
+		String actual1 = ite.translateEng(10000);
+		String expected2 = "10000未満の整数を入力してください！";
+		String actual2 = ite.translateEng(10001);
+		assertThat(actual1, is(expected1));
+		assertThat(actual2, is(expected2));
 	}
 
 }
